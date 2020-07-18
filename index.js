4fe74e1e75d43999bb59158616ec4f47d74b1fed
Akhil Seshan <akhilses@gmail.com>
2020-07-18 21:06:46 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,45 +1,11 @@
-var express = require('express')
-const app = express();
-var bodyParser = require('body-parser')
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const express = require("express");
 
-//set up view engine
-app.set('view engine', 'ejs');
+var app = express();
 
-//body parser
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// io.on('connection', (socket) => {
-//     console.log('a user connected', socket.id);
-// });
-
-//create home route
-app.get('/', (req, res) => {
-    res.render('home');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
-app.post('/getmeeting', (req, res) => {
-    var meetingId = req.body.meetingid
-    console.log(meetingId)
-    res.redirect('/'.concat(meetingId))
-});
-
-app.get('/:meetingId', (req, res) => {
-    var meetingId = req.params.meetingId
-    console.log(meetingId)
-    io.on('connection', (socket) => {
-        socket.on(meetingId, (msg) => {
-            io.emit(meetingId, msg);
-        });
-    });
-    io.of('/').in(meetingId).clients(function (error, clients) {
-        console.log(clients.length)
-    });
-    res.render('chat', { meetingId: meetingId })
-})
-
-http.listen(3000, () => {
-    console.log('app now listening on port 3000');
+app.listen(8080, () => {
+  console.log("app running on port 8080");
 });
